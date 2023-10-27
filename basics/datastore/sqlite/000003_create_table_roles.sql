d4b1fcc17f7417fb48a283483469bbb87ad5df2e
turao <lenz.arthur@gmail.com>
2023-10-26 22:19:18 -0300

    Revert "Create roles_permissions table"
    This reverts commit 2a2fe627eb907cea60db4a469eb7791b19b12db5.

--- basics/datastore/sqlite/000003_create_table_roles.sql
+++ basics/datastore/sqlite/000003_create_table_roles.sql
@@ -1,13 +1,7 @@
 CREATE TABLE IF NOT EXISTS roles(
     id VARCHAR(200) PRIMARY KEY,
     name VARCHAR(200),
+    permissions VARCHAR(200),
     created_at TIMESTAMP,
     updated_at TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS roles_permissions(
-    role_id VARCHAR(200),
-    permission_id VARCHAR(200),
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (permission_id) REFERENCES permissions(id)
 );
--- basics/src/infrastructure/sqlite/role.rs
+++ basics/src/infrastructure/sqlite/role.rs
@@ -1,12 +1,9 @@
-use std::collections::HashSet;
-
 use async_trait::async_trait;
-use sqlx::{Sqlite, FromRow, Connection};
+use sqlx::{Sqlite, FromRow};
 use sqlx::pool::Pool;
 
 use chrono::{Utc, TimeZone};
 
-use crate::domain::permissions::PermissionId;
 use crate::domain::roles::{RoleId, Role};
 use crate::domain::repositories::{Error, Repository};
 
@@ -14,8 +11,21 @@ use crate::domain::repositories::{Error, Repository};
 struct SqliteRoleRepositoryModel {
     id: String,
     name: String,
+    permissions: String,
     created_at: i64,
     updated_at: i64,
+}
+
+impl From<Role> for SqliteRoleRepositoryModel {
+    fn from(value: Role) -> Self {      
+        Self {
+            id: value.get_id().into(),
+            name: value.get_name(),
+            permissions: serde_json::to_string(&value.get_permissions()).unwrap(),
+            created_at: value.get_created_at().timestamp_millis(),
+            updated_at: value.get_updated_at().timestamp_millis(),
+        }
+    }
 }
 
 impl From<SqliteRoleRepositoryModel> for Role {
@@ -23,16 +33,11 @@ impl From<SqliteRoleRepositoryModel> for Role {
         Role::builder()
             .id(RoleId::from(value.id))
             .name(value.name)
+            .permissions(serde_json::from_str(&value.permissions).unwrap())
             .created_at(Utc.timestamp_millis_opt(value.created_at).single().unwrap_or_default())
             .updated_at(Utc.timestamp_millis_opt(value.updated_at).single().unwrap_or_default())
             .build()
     }
-}
-
-#[derive(Debug, FromRow)]
-struct SqliteRolePermissionRepositoryModel {
-    role_id: String,
-    permission_id: String,
 }
 
 #[derive(Debug)]
@@ -52,71 +57,25 @@ impl SqliteRoleRepository {
 impl Repository<RoleId, Role> for SqliteRoleRepository {
     async fn get_by_id(&self, id: RoleId) -> Result<Option<Role>, Error> {
         let mut connection = self.connection_pool.acquire().await?;
-        let mut transaction = connection.begin().await?;
-        
-        let role_query = "SELECT * FROM roles WHERE id = ?";
-        let role_model = sqlx::query_as::<_, SqliteRoleRepositoryModel>(role_query)
-            .bind::<String>(id.clone().into()) // todo: map to model PK first
-            .fetch_optional(&mut *transaction).await?
-            .expect("role not found");
-        
-        let role_permissions_query = "SELECT * FROM roles_permissions WHERE role_id = ?";
-        let role_permission_models = sqlx::query_as::<_, SqliteRolePermissionRepositoryModel>(role_permissions_query)
-            .bind::<String>(id.clone().into()) // todo: map to model PK first
-            .fetch_all(&mut *transaction).await?;
-
-        let role_permissions = role_permission_models.into_iter()
-            .map(|model| PermissionId::from(model.permission_id))
-            .collect();
-
-        let role = Role::builder()
-            .id(RoleId::from(role_model.id))
-            .name(role_model.name)
-            .created_at(Utc.timestamp_millis_opt(role_model.created_at).single().unwrap_or_default())
-            .updated_at(Utc.timestamp_millis_opt(role_model.updated_at).single().unwrap_or_default())
-            .permissions(role_permissions)
-            .build();
-
-
-        Ok(Some(role))
+        let query = "SELECT * FROM roles WHERE id = ?";
+        let role = sqlx::query_as::<_, SqliteRoleRepositoryModel>(query)
+            .bind::<String>(id.into()) // todo: map to model PK first
+            .fetch_optional(&mut *connection).await?
+            .map(Role::from);
+        Ok(role)
     }
 
     async fn save(&self, entity: Role) -> Result<(), Error> {
+        let model = SqliteRoleRepositoryModel::from(entity);
         let mut connection = self.connection_pool.acquire().await?;
-        let mut transaction = connection.begin().await?;
-
-        let model = SqliteRoleRepositoryModel {
-            id: entity.get_id().into(),
-            name: entity.get_name(),
-            created_at: entity.get_created_at().timestamp_millis(),
-            updated_at: entity.get_updated_at().timestamp_millis(),
-        };
-        let role_query = "INSERT INTO roles VALUES(?, ?, ?, ?);";
-        sqlx::query(role_query)
+        let query = "INSERT INTO roles VALUES(?, ?, ?, ?, ?);";
+        sqlx::query(query)
             .bind(model.id)
             .bind(model.name)
+            .bind(model.permissions)
             .bind(model.created_at)
             .bind(model.updated_at)
-            .execute(&mut *transaction).await?;
-
-        let role_permissions: Vec<SqliteRolePermissionRepositoryModel> = entity.get_permissions()
-            .into_iter()
-            .map(|permission_id| SqliteRolePermissionRepositoryModel{
-                role_id: entity.get_id().into(),
-                permission_id: permission_id.into(),
-            })
-            .collect();
-
-        let role_permissions_query = "INSERT INTO roles_permissions VALUES(?, ?);";
-        for model in role_permissions {
-            sqlx::query(role_permissions_query)
-                .bind(model.role_id)
-                .bind(model.permission_id)
-                .execute(&mut *transaction).await?;
-        }
-
-        transaction.commit().await?;
-
+            .execute(&mut *connection).await?;
         Ok(())
     }
 }
